243bbcbed82c17b45ef78c8040b3eb103827f271
Kajal-rajodiya <kajalrajodiya1@gmail.com>
2023-07-04 08:54:10 +0530

    Update JQuery.js

--- JQuery.js
+++ JQuery.js
@@ -1,4 +1,4 @@
-ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff/*!
+fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff/*!
  * jQuery JavaScript Library v3.6.4
  * https://jquery.com/
  *
